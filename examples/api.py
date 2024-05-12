b15fdefced405391c482ec127d1279f256573295
kyutekrap <117127078+kyutekrap@users.noreply.github.com>
2024-05-12 19:11:54 +0900

    Create api.py

---
+++ examples/api.py
@@ -0,0 +1,27 @@
+from Link import CreateFlow, CreateStep, Debugger, GetStep, Decision
+
+
+def api_example():
+    CreateFlow("Flow", [
+        CreateStep.ExampleCustomMethod("Step1", {
+            "$table": "people",
+            "$values": {
+                "name": ["Kate Park", "Peter Parker"],
+                "number": ["1234567899", "1234567899"]
+            }
+        }, Persist=True, Debug=True),
+        Debugger.log(GetStep("Step1", "affected_rows")),
+        Decision({
+            "End": GetStep("Step1", "affected_rows") == 0
+        }),
+        CreateStep.ExampleCustomMethod("Step1-1", {
+            "$table": "schools",
+            "$values": {
+                "name": ["MIT", "CIT"],
+                "location": ["E Jefferson St", "N Jefferson St"]
+            }
+        }, Debug=True)
+    ])
+
+
+api_example()
