41f429d79034399adb58c75374dcadc8aa77e126
kyutekrap <katepark1225@naver.com>
2024-08-10 00:04:44 +0900

    v0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='Link',
-    version='0.1.0',
+    version='0.1.1',
     packages=['Link', 'Link.cls', 'Link.func'],
     url='https://github.com/kyutekrap/Link'
 )
