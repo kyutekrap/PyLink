28b2878c88f8f9cfaaa60ffad2b51513024bd758
kyutekrap <katepark1225@naver.com>
2024-08-21 18:10:13 +0900

    v0.1.2

--- PyLink/GetStep.py
+++
@@ -1,9 +0,0 @@
-from .property import create_local_storage
-
-
-def GetStep(name: str):
-    """
-    :param name: Name of Step
-    :return: None, else respective value
-    """
-    return create_local_storage.get_results(name)
